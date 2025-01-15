53988338bd85fb41b7c96e39a5b4e98eb61a52ca
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-15 09:57:14 +0900

    Sudoku field

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Python Sudoku Solver
+
+# Backtracking is an algorithm based on recursion
+# The function explores all the possible choices from a given state
+# It 'backtracks' to the previous state if there is no solution
+
+grid = [
+    [5, 4, 6, 8, 9, 2, 7, 1, 3],  
+    [6, 7, 2, 1, 9, 5, 4, 8, 0],
+    [1, 9, 8, 3, 4, 7, 6, 5, 2],
+    [8, 5, 9, 7, 6, 1, 3, 2, 4],
+    [4, 2, 0, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 6, 5],
+    [9, 6, 1, 5, 3, 8, 2, 7, 0],
+    [2, 8, 7, 4, 1, 9, 5, 3, 6],
+    [3, 0, 5, 2, 8, 6, 1, 4, 9]
+]
